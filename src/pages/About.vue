992bb6095f92b352bc1e9473421b748f3c497a29
antimatter162 <antimatter5175@gmail.com>
2026-09-10 12:47:05 -0700

    Added text to About and Contact pages

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,13 @@
 <template>
   <section>
-    <h1>About</h1>
-    <p>Learn more about the tutoring services.</p>
+    <h1>About Me</h1>
+    <p>My name is Hanyu Niu, and I was born in Shanghai, China and moved to the US when I was about 6 years old. I'd
+      always had a simple interest in understanding how the world around me worked, and that's what sparked my passion
+      for physics. I started prepping for olympiad physics in 8th grade and took the f=ma preliminary exam for the
+      US national physics team in 9th grade, qualifying for the next-round USAPhO exam with a score of 19. I continued
+      to qualify for the USAPhO exam for the next 3 years, and accrued one honorable mention, one bronze medal, 
+      and one silver medal in the USAPhO exam. I've also had tutoring experience both on my own and with an organization
+      (BobTutor) in teaching physics and mathematics to middle and high school students.
+    </p>
   </section>
 </template>
--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,6 +1,11 @@
 <template>
   <section>
     <h1>Contact</h1>
-    <p>Get in touch for tutoring inquiries.</p>
+    <p>Email: antimatter5175@gmail.com
+      <br>
+      Alternate Email: antimatter162@berkeley.edu
+      <br>
+      Phone: (925)660-9885
+    </p>
   </section>
 </template>
