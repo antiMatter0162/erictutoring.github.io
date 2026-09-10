7574a62f3555ef5072126501e0a86def83cfaf82
antimatter162 <antimatter5175@gmail.com>
2026-09-10 11:46:50 -0700

    Refactor project structure and enhance styling for improved user experience

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app-layout">
     <header>
+      <h1>Eric's Physics Tutoring</h1>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -9,7 +10,11 @@
     </header>
 
     <main>
-      <RouterView />
+      <RouterView v-slot="{ Component }">
+        <Transition name="page" mode="out-in" appear>
+          <component :is="Component" />
+        </Transition>
+      </RouterView>
     </main>
   </div>
 </template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <section>
-    <h1>Eric Tutoring</h1>
-    <p>Welcome to the tutoring website.</p>
+    <h1>Welcome!</h1>
+    <p>My name is Hanyu (Eric) Niu, a freshman physics major at the University of California, Berkeley. 
+      I'm deeply passionate about physics, and I hope to help you develop a strong understanding and perhaps
+      a strong interest in the subject as well.
+    </p>
   </section>
 </template>
