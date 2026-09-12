9b29dba23815f3b658bf3409ae1e234a5e703da7
antimatter162 <antimatter5175@gmail.com>
2026-09-11 23:56:41 -0700

    Enhance About and Contact pages with additional content and styling improvements; add GitHub Actions workflow for deployment

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,13 +1,21 @@
 <template>
   <section>
     <h1>About Me</h1>
-    <p>My name is Hanyu Niu, and I was born in Shanghai, China and moved to the US when I was about 6 years old. I'd
-      always had a simple interest in understanding how the world around me worked, and that's what sparked my passion
-      for physics. I started prepping for olympiad physics in 8th grade and took the f=ma preliminary exam for the
-      US national physics team in 9th grade, qualifying for the next-round USAPhO exam with a score of 19. I continued
-      to qualify for the USAPhO exam for the next 3 years, and accrued one honorable mention, one bronze medal, 
-      and one silver medal in the USAPhO exam. I've also had tutoring experience both on my own and with an organization
-      (BobTutor) in teaching physics and mathematics to middle and high school students.
-    </p>
+    <div class="about-content">
+      <img src="./assets/portrait.png" alt="A picture of me" class="portrait">
+      <p>My name is Hanyu Niu, and I was born in Shanghai, China and moved to the US when I was about 6 years old. I'd
+        always had a simple interest in understanding how the world around me worked, and that's what sparked my passion
+        for physics. I started prepping for olympiad physics in 8th grade and took the f=ma preliminary exam for the
+        US national physics team in 9th grade, qualifying for the next-round USAPhO exam with a score of 19. I continued
+        to qualify for the USAPhO exam for the next 3 years, and accrued one honorable mention, one bronze medal,
+        and one silver medal in the USAPhO exam. I've also had tutoring experience both on my own and with an organization
+        (BobTutor) in teaching physics and mathematics to middle and high school students.
+        <br><br>
+        Outside of physics, I enjoy playing the violin occasionally, though I wouldn't say it's a huge part of my life.
+        I also enjoy creating original warship designs and bringing them to fruition with 3D modeling software, as well as
+        reading up on naval history and naval warfare doctrines. I'm also a big fan of Neon Genesis Evangelion, Hollow Knight,
+        and The Battle Cats.
+      </p>
+    </div>
   </section>
 </template>
--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -2,10 +2,14 @@
   <section>
     <h1>Contact</h1>
     <p>Email: antimatter5175@gmail.com
-      <br>
+      <br><br>
       Alternate Email: antimatter162@berkeley.edu
-      <br>
+      <br><br>
       Phone: (925)660-9885
+      <br><br>
+      Please feel free to reach out to me for any inquiries or collaborations! One request: I may be in class
+      or otherwise occupied, so please opt to text me instead of calling. I'll usually respond within 1-3 days,
+      but I'll try to respond as soon as possible. Thank you for your understanding!
     </p>
   </section>
 </template>
